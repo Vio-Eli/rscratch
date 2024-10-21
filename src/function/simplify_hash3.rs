2d281c2cdcc8e1c2fed6d18e282781725f180973
Eli Fischl <vio@ocf.berkeley.edu>
2024-10-20 23:47:07 -0700

    [vio] I fixed it?

--- src/function/simplify_hash3.rs
+++ src/function/simplify_hash3.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::ptr::hash;
 use std::sync::Arc;
 use crate::function::freeze::freeze;
 use crate::function::function::{DiscriminantId, Function};
@@ -16,8 +17,10 @@ pub struct HashVals<'f> {
     pub ptr: Arc<Function<'f>>,
     pub idx: usize,
     pub hash: u64,
-    pub mul_ptr: Option<*const f64>,
-    pub pow_ptr: Option<*const f64>,
+    pub mul_ptr: Option<Arc<Function<'f>>>,
+    pub pow_ptr: Option<Arc<Function<'f>>>,
+    // pub mul_ptr: Option<*const f64>,
+    // pub pow_ptr: Option<*const f64>,
 }
 
 #[derive(Debug)]
@@ -30,7 +33,7 @@ pub struct SimplifyReturn<'f> {
 }
 
 fn simplify_hash3<'f, 'm>(function: &'f Arc<Function<'f>>, hmap: &'m mut std::collections::HashMap<HashKeys, HashVals<'f>>, id: u8) -> Vec<SimplifyReturn<'f>> {
-    
+
     let uuid = Arc::as_ptr(function) as u64; // yeah, i need to change this lol
 
     match &**function {
@@ -52,47 +55,54 @@ fn simplify_hash3<'f, 'm>(function: &'f Arc<Function<'f>>, hmap: &'m mut std::co
                 // Simplify child and iterate through grandchildren
                 simplify_hash3(child, hmap, id).iter().for_each(|grandchild| {
                     // Hashing the power in for comparison
-                    let grandchild_add_hash = freeze(&Pow { base: grandchild.ptr.clone().into(), raised: Constant(grandchild.pow).into() });
+                    let grandchild_add_hash = freeze(&Pow { base: grandchild.ptr.clone(), raised: Constant(grandchild.pow).into() });
                     // Check if the grandchild is in the hashmap
                     if let Some(hashvals) = hmap.get_mut(&HashKeys { uuid, hash: grandchild_add_hash}) {
 
                         // If the Mul ptr exists, we can mutate the value
-                        if let Some(gc_mul_ptr) = hashvals.mul_ptr {
-                            unsafe {
-                                *(gc_mul_ptr as *mut f64) += grandchild.mul;
+                        if let Some(gc_mul_ptr) = hashvals.mul_ptr.clone() {
+                            if let Constant(val) = *gc_mul_ptr {
+                                unsafe {
+                                    let ptr = Arc::as_ptr(&gc_mul_ptr) as *mut Function;
+                                    *ptr = Constant(val + grandchild.mul);
+                                }
                             }
                         } else {
                             // Create a new Mul block and getting the pointer to the value inside it
-                            let mut new_mul_const = Constant(grandchild.mul + 1.0); // We add 1.0 because the previous value was 1.0
-                            if let Constant(ref mut val) = new_mul_const {
-                                hashvals.mul_ptr = Some(val);
-                            }
+                            let new_mul_const = Arc::new(Constant(grandchild.mul + 1.0)); // We add 1.0 because the previous value was 1.0
+                            // if let Constant(ref mut val) = new_mul_const {
+                            //     hashvals.mul_ptr = Some(val);
+                            // }
+                            hashvals.mul_ptr = Some(new_mul_const.clone());
 
                             // Splicing function tree and inserting new mul block
-                            let new_gc = Arc::new(Mul { vec: vec![hashvals.ptr.clone().into(), Arc::new(new_mul_const)] });
+                            let new_gc = Arc::new(Mul { vec: vec![hashvals.ptr.clone(), new_mul_const] });
 
                             // Inserting new Mul block into the new Add vec
                             new_add_vec[hashvals.idx] = new_gc.clone();
-                            
+
                             // Updating the hashvals
                             hashvals.ptr = new_gc;
                         }
                     } else {
                         // If the grandchild is not in the hashmap, we add it
                         new_add_vec.push(grandchild.ptr.clone());
-                        
-                        let (mul_ptr, pow_ptr) = match (&grandchild.mul, &grandchild.pow) {
+
+                        let (mul_ptr, pow_ptr) = match (grandchild.mul, grandchild.pow) {
                             (1.0, 1.0) => (None, None),
-                            (mul, 1.0) => (Some(mul as *const f64), None),
-                            (1.0, pow) => (None, Some(pow as *const f64)),
-                            (mul, pow) => (Some(mul as *const f64), Some(pow as *const f64))
+                            (mul, 1.0) => (Some(Arc::new(Constant(mul))), None),
+                            (1.0, pow) => (None, Some(Arc::new(Constant(pow)))),
+                            (mul, pow) => (Some(Arc::new(Constant(mul))), Some(Arc::new(Constant(pow))))
                         };
-                        
+
+                        println!("Created Mul Ptr: {:?}", mul_ptr);
+                        println!("Created Mul Val: {:?}", grandchild.mul);
+
                         hmap.insert(HashKeys { uuid, hash: grandchild_add_hash }, HashVals { ptr: grandchild.ptr.clone(), idx: new_add_vec.len() - 1, hash: grandchild_add_hash, mul_ptr, pow_ptr });
                     }
                 })
             });
-            
+
             let fnc = Add { vec: new_add_vec };
             let hash = freeze(&fnc);
             vec![SimplifyReturn { ptr: Arc::new(fnc), hash, id: function.id(), mul: 1.0, pow: 1.0 }]
@@ -127,10 +137,10 @@ mod tests {
     use crate::function::function::Function::{Constant, Variable, Add, Mul, S};
 
     #[test]
-    fn test_simplify_hash2() {
+    fn test_simplify_hash3() {
         let x = Variable("x");
         let y = Variable("y");
-        let z = Arc::new(x.clone() + x.clone());
+        let z = Arc::new(x.clone() + x.clone() + x.clone() + x.clone());
         let z_id = z.id();
 
         let mut hmap = HashMap::new();
